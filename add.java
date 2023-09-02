191bdbf9f7d08dafdf0805b09ee6f7c1f61a01e2
Sasankmendu <113572620+Sasankmendu@users.noreply.github.com>
2023-09-02 11:04:06 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class add
+{
+	public static void main(String args[])
+	{
+		int a=10;
+		int b=17;
+		int c=a+b;
+		System.out.println("ADDITION OF TWO NUMBERS:"+c);
+	}
+}
